2f73cc771b1c8793ed594cc6178664f1a9fcb7a3
Hexa Ghost 09 <hexaghost-09@tutamail.com>
2025-07-14 12:58:51 +0000

    Added Analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,36 @@
+import { Databuddy } from '@databuddy/sdk';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import SmartphoneRanking from "./components/SmartphoneRanking";
 import TabletRanking from "./components/TabletRanking";
 import LaptopRanking from "./components/LaptopRanking";
 
+function AppLayout({ children }) {
+  return (
+    <>
+      {children}
+      <Databuddy
+        clientId="TTktJTEnCX55gNKZ8LEy9"
+        enableBatching={true}
+      />
+    </>
+  );
+}
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen px-6 py-16 bg-gradient-to-b from-white to-gray-100 dark:from-black dark:to-[#111] text-gray-800 dark:text-gray-100">
-      <Header />
+    <AppLayout>
+      <div className="flex flex-col items-center justify-center min-h-screen px-6 py-16 bg-gradient-to-b from-white to-gray-100 dark:from-black dark:to-[#111] text-gray-800 dark:text-gray-100">
+        <Header />
 
-      <div className="flex flex-col gap-20 w-full items-center">
-        <SmartphoneRanking />
-        <TabletRanking />
-        <LaptopRanking />
+        <div className="flex flex-col gap-20 w-full items-center">
+          <SmartphoneRanking />
+          <TabletRanking />
+          <LaptopRanking />
+        </div>
+
+        <Footer />
       </div>
-
-      <Footer />
-    </div>
+    </AppLayout>
   );
 }
