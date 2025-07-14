9f3df26b57df1ef8c364157684e135c0a8073863
Hexa Ghost 09 <hexaghost-09@tutamail.com>
2025-07-14 13:02:37 +0000

    New

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,9 @@ import SmartphoneRanking from "./components/SmartphoneRanking";
 import TabletRanking from "./components/TabletRanking";
 import LaptopRanking from "./components/LaptopRanking";
 
-function AppLayout({ children }) {
-  return (
-    <>
-      {children}
-      <Databuddy
-        clientId="TTktJTEnCX55gNKZ8LEy9"
-        enableBatching={true}
-      />
-    </>
-  );
-}
-
 export default function Home() {
   return (
-    <AppLayout>
+    <>
       <div className="flex flex-col items-center justify-center min-h-screen px-6 py-16 bg-gradient-to-b from-white to-gray-100 dark:from-black dark:to-[#111] text-gray-800 dark:text-gray-100">
         <Header />
 
@@ -31,6 +19,11 @@ export default function Home() {
 
         <Footer />
       </div>
-    </AppLayout>
+
+      <Databuddy
+        clientId="TTktJTEnCX55gNKZ8LEy9"
+        enableBatching={true}
+      />
+    </>
   );
 }
